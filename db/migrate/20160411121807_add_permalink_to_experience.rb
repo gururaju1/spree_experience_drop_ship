45f254d056f81b394a337ac75e6d182a5fa3ecb2
gururaju <gururaju57@gmail.com>
2016-04-11 14:02:37 +0000

    first commit

---
+++ db/migrate/20160411121807_add_permalink_to_experience.rb
@@ -0,0 +1,6 @@
+class AddPermalinkToExperience < ActiveRecord::Migration
+  def change
+    add_column :spree_experiences, :slug, :string
+    add_index :spree_experiences, :slug, unique: true
+  end
+end
